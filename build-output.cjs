b5e1bb7735f5ad3c2c3134ff4f761f37c8f3e425
Lars Vorreiter <lars.vorreiter@me.com>
2023-09-26 13:34:40 +0200

    changed token

--- build-output.cjs
+++ build-output.cjs
@@ -22,44 +22,32 @@ registerTransforms(StyleDictionary);
 
 
 const sd = StyleDictionary.extend({
-    source: ['**/*.tokens.json'],
-    platforms: {
-        js: {
-            transformGroup: 'tokens-studio',
-            buildPath: 'token-build/js/',
-            files: [
-                {
-                    destination: 'variables.js',
-                    format: 'javascript/es6',
-                },
-            ],
-        },
-        css: {
-        transforms: [
-            'ts/descriptionToComment',
-            'ts/size/px',
-            'ts/opacity',
-            'ts/size/lineheight',
-            'ts/typography/fontWeight',
-            'ts/resolveMath',
-            'ts/size/css/letterspacing',
-            'ts/typography/css/fontFamily',
-            'ts/typography/css/shorthand',
-            'ts/border/css/shorthand',
-            'ts/shadow/css/shorthand',
-            'ts/color/css/hexrgba',
-            'ts/color/modifiers',
-            'name/cti/kebab',
-        ],
-        buildPath: 'token-build/css/',
-        files: [
+    "source": [
+        "**/*.tokens.json"
+      ],
+      "platforms": {
+        "css": {
+          "buildPath": "token-build/css/",
+          "prefix": "sd",
+          "transformGroup": "tokens-studio",
+          "files": [
             {
-                destination: 'variables.css',
-                format: 'css/variables',
-            },
-        ],
+              "destination": "variables.css",
+              "format": "css/variables"
+            }
+          ]
         },
-    },
+        "js": {
+          "buildPath": "token-build/js/",
+          "transformGroup": "tokens-studio",
+          "files": [
+            {
+              "destination": "variables.js",
+              "format": "javascript/es6"
+            }
+          ]
+        }
+      }
 });
 
 sd.cleanAllPlatforms();
